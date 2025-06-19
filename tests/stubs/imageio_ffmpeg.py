5afed99e77cf40b6df3ff25a497ff793c8c566b2
HARRY FOX <132673262+HARRYFOXAUS@users.noreply.github.com>
2025-06-19 15:01:31 +1000

    Add lightweight tests and agent instructions

---
+++ tests/stubs/imageio_ffmpeg.py
@@ -0,0 +1,7 @@
+# minimal stub for testing
+
+def get_ffmpeg_exe():
+    return None
+
+def get_ffprobe_exe():
+    return None
---
+++ tests/stubs/numpy.py
@@ -0,0 +1,5 @@
+class ndarray:
+    def __init__(self, value=0):
+        self.value = value
+    def squeeze(self):
+        return self.value
